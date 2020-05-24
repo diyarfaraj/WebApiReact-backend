1bac7fbbac13954ab2fa9bc9b7444dc49e5a08a0
Diyar Faraj <diyar.faraj@gmail.com>
2020-05-24 13:34:03 +0200

    git init

---
+++ WebbApiReact/Migrations/20200524090223_InitialUpdate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebbApiReact.Migrations
+{
+    public partial class InitialUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DbCandidate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fullname = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Mobile = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    BloodGroup = table.Column<string>(type: "nvarchar(3)", nullable: true),
+                    Adress = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DbCandidate", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DbCandidate");
+        }
+    }
+}
---
+++ WebbApiReact/Models/DCandidate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebbApiReact.Models
+{
+    public class DCandidate
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Fullname { get; set; }
+        [Column(TypeName = "nvarchar(16)")]
+        public string Mobile { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+        public int Age { get; set; }
+        [Column(TypeName = "nvarchar(3)")]
+        public string BloodGroup { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Adress { get; set; }
+
+    }
+}
